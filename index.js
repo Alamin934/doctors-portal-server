df650458da1a76cd5612f2c259121ede8b0902b9
Alamin934 <islamalamin934@gmail.com>
2022-06-29 12:31:04 +0600

    create post method for send appointments data to database

--- index.js
+++ index.js
@@ -8,11 +8,40 @@ const port = process.env.PORT || 5000;
 
 //MiddleWare
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ogrrwih.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+async function run() {
+
+    try {
+        await client.connect();
+
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+
+        //Appointments POST method
+        app.post('/appointments', async (req, res) => {
+            const appointment = req.body;
+            const result = await appointmentsCollection.insertOne(appointment);
+            res.json(result)
+        });
+
+
+    }
+    finally {
+        // await client.close();
+    }
+
+
+
+
+
+}
+run().catch(console.dir);
 
 
 
